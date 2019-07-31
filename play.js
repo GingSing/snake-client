6222da94f638596416529ec375ff3dc6a4626b3e
Gary <garyyue2017@gmail.com>
2019-07-31 20:33:24 +0000

    updated the controls

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function(callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', data => callback(data));
+  return stdin;
+};
+
+const handleUserInput = function(command) {
+  if(command === '\u0003'){
+    return process.exit();
+  }
+  return command;
+};
+
+setupInput(handleUserInput);

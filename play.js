a892d2b2d462ddc7cdcbe055c456d23b37eb8685
Gary <garyyue2017@gmail.com>
2019-07-31 20:38:44 +0000

    put input into new file

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
-
-const setupInput = function(callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', data => callback(data));
-  return stdin;
-};
-
-const handleUserInput = function(command) {
-  if(command === '\u0003'){
-    return process.exit();
-  }
-  return command;
-};
-
-setupInput(handleUserInput);
+setupInput();

280fb43ae6510e8f87a4290f7bcfaa87058dc875
Gary <garyyue2017@gmail.com>
2019-07-31 20:49:17 +0000

    added movement for snake

--- input.js
+++ input.js
@@ -1,17 +1,36 @@
-const setupInput = function(callback) {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', data => callback(data));
+  stdin.on('data', data => handleUserInput(data, connection));
   return stdin;
 };
 
-const handleUserInput = function(command) {
+const handleUserInput = function(command, conn) {
   if(command === '\u0003'){
     return process.exit();
   }
+
+  switch(command){
+    case "w":
+      conn.write("Move: up");
+      break;
+    case "a":
+      conn.write("Move: left");
+      break;
+    case "s":
+      conn.write("Move: down");
+      break;
+    case "d":
+      conn.write("Move: right");
+      break;
+  }
+
   return command;
 };
 
-module.exports = { setupInput : () => setupInput(handleUserInput) }
+module.exports = { setupInput }

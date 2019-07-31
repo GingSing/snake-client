dc5de66dc05dd750860746b74b8d8e3f3f71de14
Gary <garyyue2017@gmail.com>
2019-07-31 20:09:18 +0000

    sent name and added event listenter

--- client.js
+++ client.js
@@ -10,6 +10,11 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    conn.write("Name: GRY");
+    console.log(`Successfully connected to game server`);
+  })
+
   conn.on('data', data => {
     console.log(data);
   })
